17dfeaf147583f192da496962b8b2288e554119e
hakim112211 <maulanahakimrizki@gmail.com>
2023-07-24 15:49:12 +0700

    menambahkan video 2

--- contacts.js
+++ contacts.js
@@ -1,12 +1,7 @@
-const { rejects } = require('assert');
 const fs = require('fs');
-const { resolve } = require('path');
-const readline = require('readline');
+const chalk = require('chalk');
+const validator = require('validator')
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
 // membuat folder data jika belum ada
 const dirpath = './data';
@@ -20,25 +15,47 @@ if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, '[]', 'utf-8');
 }
 
-const tulispertanyaan = (pertanyaan) => {
-  return new Promise((resolve, reject)=> {
-rl.question(pertanyaan, (nama) => {
-   resolve(nama);
-    });
-  });
-};
+
 
 const simpanContact = (nama, email, noHP) => {
     const contact = { nama, email, noHP };
     const fileBuffer = fs.readFileSync('data/contacts.json', 'utf-8');
     const contacts = JSON.parse(fileBuffer);
 
+    // cek duplikat
+    const duplikat = contacts.find((contact) => contact.nama === nama);
+    if(duplikat) {
+      console.log(
+        chalk.red.inverse.bold('Contact sudah terdaftar, gunakan nama lain!')
+      );
+      return false;
+    }
+
+
+    // cek email
+    if (email) {
+      if (!validator.isEmail(email)) {
+        console.log(chalk.red.inverse.bold('Email tidak valid!'));
+        return false;
+      }
+    }
+
+    // cek no HP
+    if (email) {
+      if (!validator.isMobilePhone(noHP, 'id-ID')) {
+        console.log(chalk.red.inverse.bold('Nomor HP tidak valid!'));
+        return false;
+      }
+    }
+
+
+
     contacts.push(contact);
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
 
-    console.log('terimakasih sudah memasukkan data.');
-    rl.close();
+    console.log(chalk.green.inverse.bold('terimakasih sudah memasukkan data.'));
+    
 };
 
-module.exports = { tulispertanyaan, simpanContact}
+module.exports = { simpanContact}
